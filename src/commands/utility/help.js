4912a0588d8c4cb9278f648f884c24b2bfd8023d
j-dogcoder <j.dogcoder@gmail.com>
2021-05-12 15:03:08 -0400

    :dizzy: Inital commit

--- src/commands/utility/help.js
+++ src/commands/utility/help.js
@@ -13,7 +13,7 @@ exports.run = async (client, message, args) => {
   client.embed.send(message, {
     title: 'Help',
     code: true,
-    desc: `The bot was created by **${client.users.cache.get(client.config.ownerID).tag}**, if you have any questions or would like to suggest new features or report bugs, please send them a direct message. All commands start with \`${client.config.prefix}\`.`,
+    desc: `The bot was created by **${client.users.cache.get(client.config.ownerID)}**, if you have any questions or would like to suggest new features or report bugs, please send them a direct message. All commands start with \`${client.config.prefix}\`.`,
     fields: [{
       name: 'invite',
       value: 'A way to invite this bot to your own guild.'
